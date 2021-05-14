f9b8e2e9361a9365960ba0ec2dd1f6c7877159ee
Timothy Middelkoop <tmiddelkoop@internet2.edu>
2021-05-14 13:14:31 -0500

    Merge branch and do trival renames and fixes

--- slurm-gcp/setup.sh
+++ slurm-gcp/setup.sh
@@ -3,4 +3,5 @@
 gcloud compute ssh class-controller -- < scripts/controller.sh
 
 gcloud compute ssh class-controller -- sudo -i < scripts/singularity.sh
+gcloud compute ssh class-controller -- sudo -i < scripts/install_miniconda.sh
 gcloud compute ssh class-controller -- < scripts/spack.sh
