945a571648f7b9d5d635c0dc1ea098d54b3c4998
Tony <montereytony@gmail.com>
2021-05-15 12:41:31 -0700

    Update setup.sh
    When running this script, gcloud failed asking for the --zone variable, so I added it to the script in case someone else runs into this issue. It also asks for the password each time it does an ssh to the controller.  I am wondering if we should try to combine this into one step so we can minimize user intervention.

--- slurm-gcp/setup.sh
+++ slurm-gcp/setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-gcloud compute ssh class-controller -- < scripts/controller.sh
+#
+# This script installs kubernetes and apps listed below on slurm cluster
+#
+# not sure why I had to add the ZONE variable to this for me to run,
+# I see that it is defined in the other scripts. It could be my version of
+# gcloud
+#
 
-gcloud compute ssh class-controller -- sudo -i < scripts/singularity.sh
-gcloud compute ssh class-controller -- sudo -i < scripts/install_miniconda.sh
-gcloud compute ssh class-controller -- < scripts/spack.sh
+ZONE="us-central1-c"
+
+gcloud compute ssh class-controller --zone=$ZONE  -- < scripts/controller.sh
+
+gcloud compute ssh class-controller --zone=$ZONE -- sudo -i < scripts/singularity.sh
+gcloud compute ssh class-controller --zone=$ZONE -- sudo -i < scripts/install_miniconda.sh
+gcloud compute ssh class-controller --zone=$ZONE -- < scripts/spack.sh
